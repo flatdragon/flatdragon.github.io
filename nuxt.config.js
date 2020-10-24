138965d53b53ad1f4571cb69174f7b3f5c44e655
Kochetygov Andrew <flatdragon@yandex.ru>
2020-10-24 23:11:06 +0700

    add github workflow and nuxt.js scaffolding

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+import { author, description } from './package.json'
+
+export default {
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + author,
+    title: description || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#FF9800' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout fill-height column align-center justify-center>
+    <h1>{{ author }}</h1>
+    <small>{{ description }}</small>
+  </v-layout>
+</template>
+
+<script>
+import { author, description } from '~/package.json'
+
+export default {
+  computed: {
+    author: () => author,
+    description: () => description
+  }
+}
+</script>
